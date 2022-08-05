3b4b2661897bc1d378330e637b60f28b4e5c2270
Tinembart-SUDO <miguel.tinembart@rebsamen.net>
2022-08-05 12:32:16 +0200

    Finished assignment etch-a-sketch
    Grid is drawable
    You can change color mid drawing
    you can change grid size

--- script.js
+++ script.js
@@ -1,92 +1,89 @@
-let gridSizeX = 16
-let gridSizeY = gridSizeX;
-let isHover = false
-let isKlicked = false
-const hoverStyle = document.createElement('style');
-hoverStyle.innerHTML = `.hover {background-color: 'black';}`;
-document.body.appendChild(hoverStyle)
-let color = getRandomColor();
+const etchASketch = {}
 
-//Setup canvas inside of container element(div) Also when click is registered inside canvas, setup an event
-const canvas = document.querySelector("div.container")
-canvas.addEventListener('mousedown', () => {
-    isKlicked = true
-    console.log("isKlicked is " + isKlicked)
-})
+etchASketch.grid = {}
+etchASketch.init = false
+etchASketch.grid.size = 16
+etchASketch.grid.color = 'black'
+etchASketch.grid.tileList = function (size) {
+    size = size * size
+    const gridArray = Array.from(Array(size).keys())
+    return gridArray
+}
+etchASketch.isKlicked = false
+etchASketch.isHover = false
+etchASketch.grid.canvas = document.querySelector("div.container")
 
-canvas.addEventListener('mouseup', () => {
-    isKlicked = false
-    console.log("isKlicked is " + isKlicked)
-})
+etchASketch.grid.createTile = function () {
 
+    //find variable name pls...
+    let tile = document.createElement('div');
+    tile.classList.add("grid");
+    tile.style.cssText += 'background-color: white'
 
-// Running Section
-applyRandomButton()
-applyGridSizeButton()
-creategrid(16, 16)
+    tile.addEventListener('mousedown', (e) => {
+        e.target.style.cssText = `background-color: ${this.color}`
+    })
 
-
-//Function declaration Section
-
-//creates grid with x amount of objects 
-//gets its x and y coordinates from variable in declaration block
-//while looping through add eventlisteners and class
-function creategrid(x, y) {
-    let createLength = x * y
-    for (let index = 1; index < createLength; index++) {
-        grid = document.createElement("div");
-        grid.classList.add("grid");
-        grid.style.cssText += 'background-color: white'
-        //add mousedown so first div gets also painted
-        grid.addEventListener('mousedown', (e) => {
-            e.target.style.cssText = `background-color: ${color}`
-        })
-
-        grid.addEventListener('mouseover', (e) => {
-            if (isKlicked === true) {
-                e.target.style.cssText = `background-color: ${color}`
-                console.log("isHover is " + isHover)
-            }})
-
-        grid.addEventListener('', () => {
-                isHover = false
-                console.log("isHover is " + isHover)
-            })
-            canvas.append(grid);
+    tile.addEventListener('mouseover', (e) => {
+        if (etchASketch.isKlicked === true) {
+            e.target.style.cssText = `background-color: ${this.color}`
         }
-    }
+    })
+
+    this.canvas.append(tile)
+}
+
+etchASketch.grid.createGrid = function (size) {
+
+    let r = document.documentElement
+    r.style.setProperty('--gridsize', size)
+
+    this.canvas.addEventListener('mousedown', () => {
+        etchASketch.isKlicked = true
+    })
+
+    etchASketch.grid.canvas.addEventListener('mouseup', () => {
+        etchASketch.isKlicked = false
+    })
+
+    let myArray = this.tileList(size);
+    myArray.forEach(tile => etchASketch.grid.createTile('#FFFFFF'))
+    etchASketch.init = true
+}
 
 
-    function getRandomColor() {
-        let maxVal = 0xFFFFFF; // 16777215
-        let randomNumber = Math.random() * maxVal;  // returns a floating point random number 
-        randomNumber = Math.floor(randomNumber)
-        randomNumber = randomNumber.toString(16);
-        let randColor = randomNumber.padStart(6, 0);
-        return `#${randColor.toUpperCase()}`
-    }
+etchASketch.applyRandomButton = function () {
+    const randomButton = document.getElementById("randomColorBtn");
+    randomButton.addEventListener('click', () => {
+        this.grid.color = getRandomColor()
+    })
+}
 
-    function makeHoverClass() {
-        removeHoverClass();
-        const newColor = getRandomColor();
-        hoverStyle.innerHTML = `.hover {background-color: ${newColor};}`;
-        document.body.appendChild(hoverStyle);
-    }
+etchASketch.applyGridSizeButton = function () {
+    const gridSizeButton = document.getElementById("promptGridSize");
+    gridSizeButton.addEventListener('click', () => {
+        let myprompt = prompt("GridSize?")
+        etchASketch.reload(myprompt)
+    })
+}
 
-    function removeHoverClass() {
-        document.body.removeChild(hoverStyle)
-    }
+etchASketch.reload = function (size) {
+    if (!size) {return}
+    while(this.grid.canvas.firstChild) {
+    this.grid.canvas.removeChild(this.grid.canvas.firstChild)
+}
+etchASketch.grid.createGrid(size)
+}
 
-    function applyRandomButton() {
-        const randomButton = document.getElementById("randomColorBtn");
-        randomButton.addEventListener('click', () => {
-            color = getRandomColor()
-        })
-    }
+etchASketch.grid.createGrid(etchASketch.grid.size)
+etchASketch.applyRandomButton()
+etchASketch.applyGridSizeButton()
 
-    function applyGridSizeButton() {
-        const gridSizeButton = document.getElementById("promptGridSize");
-        gridSizeButton.addEventListener('click', () => {
-            gridSizeX = Number(prompt("What grid size would you like", "16"))
-        })
-    }
+function getRandomColor() {
+    let maxVal = 0xFFFFFF; // 16777215
+    let randomNumber = Math.random() * maxVal;  // returns a floating point random number 
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);
+    return `#${randColor.toUpperCase()}`
+}
